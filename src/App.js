aff126d3b814bff7e7794772d48404619e2ccf6b
Jaimin Shah <66469510+jayyymin18@users.noreply.github.com>
2022-06-21 17:28:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import SortingVisualizer from './sortingvisualizer/sortingvisualizer'
 function App() {
   return (
-     <Text>{`
-      line 1
-      line 2
-     `}</Text>
     <SortingVisualizer />
   );
 }
